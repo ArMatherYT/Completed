63d180fa3b989b9f4e850818625ecf70052d2a36
ArMatherYT <armaferdraws@gmail.com>
2024-01-01 14:10:22 -0600

    Tictactoe

--- Shorts/tictactoe_short.py
+++ Shorts/tictactoe_short.py
@@ -1,14 +1,13 @@
 import pygame as pg
-
 DISP,_=pg.display.set_mode((480,480)),pg.init()
 BD, P, CNT = ['','','','','','','','',''], 'X', 0
 PS,FONT = {'X':'O','O':'X'},pg.font.SysFont('Arial',160)
-
 while True:
     DISP.fill('black')
     if CNT==9: CNT,BD=0,['','','','','','','','','']
     for e in pg.event.get():
         if e.type==pg.QUIT: exit()
+        if e.type==pg.KEYDOWN: BD=['','','','','','','','','']
         if e.type==pg.MOUSEBUTTONDOWN:
             i = int(e.pos[0]/160) + 3*int(e.pos[1]/160)
             if not BD[i]: BD[i],P,CNT=P,PS[P],CNT+1
